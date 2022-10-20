1e30f591f3b43a1a27c466f393f86dbfc176fc8f
pajecawav <pajecawav@gmail.com>
2022-10-20 22:48:46 +0300

    add empty habits list placeholder

--- src/components/HabitsList/index.tsx
+++ src/components/HabitsList/index.tsx
@@ -3,6 +3,14 @@ import { observer } from "mobx-react-lite";
 import { HabitListEntry } from "./HabitsListEntry";
 
 export const HabitsList = observer(() => {
+	if (store.habits.length === 0) {
+		return (
+			<div className="h-32 grid place-items-center text-xl text-neutral-500 border-b border-b-neutral-700">
+				No habits in the list.
+			</div>
+		);
+	}
+
 	return (
 		<div className="text-sm sm:text-base space-y-2">
 			{store.habits.map(habit => (
