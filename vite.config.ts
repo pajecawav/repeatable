717645d5751b2eed865e553dc47796a374fdaa2b
pajecawav <pajecawav@gmail.com>
2022-11-15 19:51:13 +0300

    convert to PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,47 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { defineConfig } from "vite";
+import { ManifestOptions, VitePWA } from "vite-plugin-pwa";
+
+const manifest: Partial<ManifestOptions> = {
+	name: "Repeatable",
+	short_name: "Repeatable",
+	description: "Habit tracker PWA",
+	icons: [
+		{
+			src: "/android-chrome-192x192.png",
+			sizes: "192x192",
+			type: "image/png",
+		},
+		{
+			src: "/android-chrome-512x512.png",
+			sizes: "512x512",
+			type: "image/png",
+		},
+		{
+			src: "/android-chrome-512x512.png",
+			sizes: "512x512",
+			type: "image/png",
+			purpose: "any maskable",
+		},
+	],
+	theme_color: "#000000",
+	background_color: "#ffffff",
+	display: "standalone",
+	orientation: "portrait",
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+		react(),
+		VitePWA({
+			manifest,
+			workbox: {
+				globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+			},
+		}),
+	],
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
